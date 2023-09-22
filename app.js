a52819a7b34e7372e6ba1578d88621209052cbb0
Kapil Kharera <kapil95881@gmail.com>
2023-09-22 12:30:55 +0530

    Render first view for allpost

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
+
+const blogRoutes = require("./routes/blog.routes");
 
 const app = express();
+
+//activating ejs view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+
+app.use(blogRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
