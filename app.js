55e5d3ebf0705bd3ca8455c562b533fa9960ad3e
Kapil Kharera <kapil95881@gmail.com>
2023-09-24 15:13:12 +0530

    Added views for error handling

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(express.static("public"));
 
 app.use(blogRoutes);
 
+app.use(function(err, req, res, next) {
+    console.log("Error : ", err);
+    res.status(500).render('500');
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
